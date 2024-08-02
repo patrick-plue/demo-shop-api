f9c934aa2ac173aa141f279cc57e3d4ef945e11e
Patrick Pluequett <ppluequett@gmail.com>
2024-08-02 13:57:31 +0200

    adding zod schemas

--- src/zod/schemas.ts
+++ src/zod/schemas.ts
@@ -6,10 +6,18 @@ export const userSchema = z.object({
     password: z.string().min(8).trim(),
 });
 
-export const productSchema = z.object({});
+export const productSchema = z.object({
+    name: z.string().trim().min(2).max(50).min(2),
+    description: z.string().trim().min(2),
+    price: z.number().positive(),
+    categoryId: z.number(),
+});
 
 export const categorySchema = z.object({
     name: z.string().trim().min(2).max(20).toLowerCase(),
 });
 
-export const orderSchema = z.object({});
+export const orderSchema = z.object({
+    userId: z.number(),
+    total: z.number().positive(),
+});
