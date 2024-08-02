bfb1da93fb28203772ec2629cac8f4aafe06280c
Patrick Pluequett <ppluequett@gmail.com>
2024-08-02 17:42:03 +0200

    add check order and user middleware

--- src/middlewares/checkIfExits.ts
+++ src/middlewares/checkIfExits.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import db from '../db';
-import { category } from '../db/schema';
+import { category, product, order, user } from '../db/schema';
 import { eq } from 'drizzle-orm';
 import ErrorResponse from '../utils/ErrorResponse';
-import { CategoryRequest, ProductRequest } from '../types/requests';
+import {
+    CategoryRequest,
+    OrderRequest,
+    ProductRequest,
+    UserRequest,
+} from '../types/requests';
 
 export async function checkIfCategoryExists(
     req: CategoryRequest,
@@ -29,10 +34,44 @@ export async function checkIfProductExists(
 ) {
     try {
         const result = await db.query.product.findFirst({
-            where: eq(category.id, +req.params.id),
+            where: eq(product.id, +req.params.id),
         });
         if (!result) throw new ErrorResponse('product not found', 404);
         req.product = result;
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function checkIfOrderExists(
+    req: OrderRequest,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const result = await db.query.order.findFirst({
+            where: eq(order.id, +req.params.id),
+        });
+        if (!result) throw new ErrorResponse('order not found', 404);
+        req.order = result;
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function checkIfUserExists(
+    req: UserRequest,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const result = await db.query.user.findFirst({
+            where: eq(user.id, +req.params.id),
+        });
+        if (!result) throw new ErrorResponse('user not found', 404);
+        req.user = result;
         next();
     } catch (error) {
         next(error);
